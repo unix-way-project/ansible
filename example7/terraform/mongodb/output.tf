e569705505b8186fb22e2d9301bdf3f58e6e5302
Evgeny Shtranvasser <jackalsh@gmail.com>
2021-09-01 18:47:24 +0300

    example7 moved to separate project

--- example7/terraform/mongodb/output.tf
+++
@@ -1,8 +0,0 @@
-
-output "rs_cfg" {
-  value = module.rs_cfg
-}
-
-output "rs_data" {
-  value = module.rs_data
-}
--- example7/terraform/output.tf
+++
@@ -1,8 +0,0 @@
-
-output "unixway_mongodb" {
-  value = module.unixway_mongodb
-}
-
-output "unixway_load_balancer" {
-  value = module.unixway_load_balancer
-}
